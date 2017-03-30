e27f4b691cf3bc753433f635571ca0895f8552fd
sail741 <leo.mullot@gmail.com>
2017-03-30 09:28:38 +0200

    route login sans passer par 2 redirections

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,18 +2,11 @@ var passport = require('passport');
 module.exports = function(app){
 
 	//post pour la connection
-	app.post('/login', passport.authenticate('local', {
-			successRedirect: '/loginOK',
-			failureRedirect: '/loginFAIL'
-		})
-	);
-
-	app.get('/loginOK', function(req, res){
-		return res.json({status: "success"});
-	})
-
-	app.get('/loginFAIL', function(req, res){
-		return res.json({status: "error"});
-	})
-
+	app.post('/login', function(req, res, next) {
+		passport.authenticate('local', function(err, user, info, status) {
+			if (err) { return res.json({status: "error"}); }
+			if (!user) { return res.json({status: "error"}); }
+			return res.json({status: "success"});
+		})(req, res, next);
+	});
 }

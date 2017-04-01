17f54220218b1987c29cf018b98428fe2aa10065
Mathieu "ralmn" HIREL <contact.ralmn@gmail.com>
2017-04-01 23:54:57 +0200

    Fix bug starts

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const flash = require('connect-flash');
+const fs = require('fs');
 
 const morgan       = require('morgan');
 const cookieParser = require('cookie-parser');

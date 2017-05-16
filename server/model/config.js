4e2f5528f5191029ef87680a390bc49de935d13a
Jean Kevin <rasen414@gmail.com>
2017-05-16 16:56:55 +0200

    limite sur le classement

--- server/model/config.js
+++ server/model/config.js
@@ -2,19 +2,19 @@ const Sequelize = require('sequelize');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-sequelize = new Sequelize('cursed_stones', 'root', 'root', {
-        host: '128.0.0.3',
-		dialect: 'mysql',
-		logging: false,
-		define: {
-        	timestamps: false
-    	}
+sequelize = new Sequelize('cursed_stones', 'cursed_stones', 'cursed_stones', {
+    host: '164.132.96.202',
+    dialect: 'mysql',
+    logging: false,
+    define: {
+        timestamps: false
+    }
 });
 
 exports.sequelizeStore = new SequelizeStore({
-	db: sequelize,
-	checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
-  expiration: 24 * 60 * 60 * 1000
+    db: sequelize,
+    checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
+    expiration: 24 * 60 * 60 * 1000
 });
 
 exports.sequelize = sequelize;

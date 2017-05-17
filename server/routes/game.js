03da9db7e66f66bda67ad2823016f10bc2e0bbdd
Mathieu "ralmn" HIREL <contact.ralmn@gmail.com>
2017-05-17 09:50:28 +0200

    routes: suppression de /game

--- server/routes/game.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-	app.get('/game', function(req, res){
-		res.render('game.ejs')
-	});
-}
--- server/routes/index.js
+++ server/routes/index.js
@@ -11,8 +11,6 @@ module.exports = function(app){
 	// One page for presentation / login / register
 	require('./home')(app);
 
-	// Game
-	require('./game')(app);
 
 	// Test for Leo
 	require('./add_card')(app);
